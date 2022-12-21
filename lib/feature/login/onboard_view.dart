072bf22a2f668dae04ea2d1be1828a7153340eb7
buseakyuz <buse.akyuz29@gmail.com>
2022-12-22 02:36:32 +0300

    onboard view done

--- lib/feature/login/onboard_view.dart
+++ lib/feature/login/onboard_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:remindly/feature/login/login_view.dart';
 
 class OnboardView extends StatefulWidget {
   const OnboardView({super.key});
@@ -12,6 +13,82 @@ class OnboardView extends StatefulWidget {
 class _OnboardViewState extends State<OnboardView> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(children: [
+        Expanded(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Image.asset("assets/images/remindnote.png"),
+            ],
+          ),
+        ),
+        Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(
+              top: Radius.circular(44.0),
+            ),
+          ),
+          margin: EdgeInsets.zero,
+          color: Colors.black,
+          child: Container(
+            width: double.infinity,
+            child: SafeArea(
+                top: false,
+                child: Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        height: 24.0,
+                      ),
+                      Text(
+                        "Geleceğe not bırak",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 32.0,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: 12.0,
+                      ),
+                      Text(
+                        "Notunu oluştur, hatırlatalım.",
+                        style: TextStyle(color: Colors.white, fontSize: 19.0),
+                      ),
+                      SizedBox(
+                        height: 52.0,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => LoginView()));
+                        },
+                        child: Container(
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10.0)),
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Center(
+                                child: Text("Hadi Başlayalım",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 18.0))),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 24.0,
+                      ),
+                    ],
+                  ),
+                )),
+          ),
+        )
+      ]),
+    );
   }
 }
